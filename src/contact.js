026576051ecda2ca2af068aba38d6995b19f21f9
nronzel <86695181+nronzel@users.noreply.github.com>
2022-12-17 14:46:44 -0500

    fixed typo

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,7 @@
 const updateNav = () => {
   const home = document.querySelector(".home")
   const menu = document.querySelector(".menu")
-  const contact = document.querySelectoru(".contact")
+  const contact = document.querySelector(".contact")
 
   home.classList.remove("tab-active")
   menu.classList.remove("tab-active")
